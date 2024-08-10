55f85542f5b013c63e59a40847dd8baf4a28f340
amanjoshi <ajoshi373@gmail.com>
2024-08-10 23:25:12 +0530

    Logging

--- src/main/java/com/example/auditing/client/impl/EmployeeClientImpl.java
+++ src/main/java/com/example/auditing/client/impl/EmployeeClientImpl.java
@@ -4,6 +4,8 @@ import com.example.auditing.advice.ApiResponse;
 import com.example.auditing.client.EmployeeClient;
 import com.example.auditing.dto.EmployeeDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -16,23 +18,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeClientImpl implements EmployeeClient {
 
+    Logger log= LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     private final RestClient restClient;
 
     @Override
     public List<EmployeeDto> getAllEmployee() {
         try {
+            log.trace("Attempting to get All employees");
 
             ApiResponse<List<EmployeeDto>> employeeList = restClient.get()
                     .uri("/employee/findAllEmployee")
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
-                        System.out.println("Client error");
+                        log.error("Client error");
                     })
                     .body(new ParameterizedTypeReference<>() {
                     });
             //use .toEntity if we want Api Response
             return employeeList.getData();
         } catch (Exception e) {
+            log.error("Error:{}",e);
             throw new RuntimeException(e);
         }
 
